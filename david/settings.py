cc74c0649522f01f76323f5036a22b204a6207f5
Devang Kamble <risingentropy20@gmail.com>
2020-09-19 22:41:12 +0530

    Whitenoise

--- david/settings.py
+++ david/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +138,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statics')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
